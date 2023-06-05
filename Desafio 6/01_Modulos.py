b5ef4bf3d31302645c36e18cdf382257f61d6d04
Miguel Sousa Morais <msousamorais01@gmail.com>
2023-06-05 16:11:51 -0300

    #007 - Bibliotecas

--- Desafio 6/01_Modulos.py
+++ Desafio 6/01_Modulos.py
@@ -5,18 +5,24 @@ import sys
 # Printar o Sistema Operacional que voce esta usando:
 # YOUR CODE HERE
 
+print("Sistema Operacional:", sys.platform)
+
 # Printar a versão de Python que voce esta usando:
 # YOUR CODE HERE
 
+print("Versão do Python:", sys.version)
 
 import os
 # See the docs for the OS module: https://docs.python.org/3.7/library/os.html
 
 # Printar a process Id atual
 # YOUR CODE HERE
+print("ID do Processo:", os.getpid())
 
 # Printar o atual diretório:
 # YOUR CODE HERE
+print("Diretório Atual:", os.getcwd())
 
 # Printar o nome da maquina
 # YOUR CODE HERE
+print("Nome da Máquina:", os.uname().nodename)
--- Desafio 6/02_Numpy.py
+++ Desafio 6/02_Numpy.py
@@ -3,16 +3,23 @@ import numpy as np
 
 #1- Crie uma matriz 1D com números de 0 a 9
 
+matriz_1 = np.array([0,1, 2, 3, 4, 5, 6, 7, 8, 9]) 
+print("Matriz 1: ",matriz_1)
 
 #2- Crie uma matriz booleana numpy 3×3 com ‘True’
 
+matriz_2 = np.full([3,3], True , dtype = bool) 
+print("Matriz 2: ",matriz_2)
 
 #3- Extraia todos os números ímpares de ‘arr’
 
 arr = np.array([0, 1, 2, 3, 4, 5, 6, 7, 8, 9])
+matriz_impares = arr[arr % 2 != 0]
+print("Matriz_ímpar: ",matriz_impares)
 
 #4- Substitua todos os números ímpares arr por -1
-
+arr[arr % 2 != 0] = -1
+print("Matriz 'arr' com -1:", arr)
 
 #5- Substitua todos os números ímpares em arr com -1 sem alterar arr
 
@@ -22,15 +29,18 @@ arr = np.arange(10)
 #6- Converta uma matriz 1D para uma matriz 2D com 2 linhas:
 
 arr = np.arange(10)
+matriz_2D = np.reshape(arr, (2, -2))
+print("Matriz 2D: ", matriz_2D)
 
        
 #6- Empilhe matrizes verticalmente:
 
 a = np.arange(10).reshape(2,-1)
 b = np.repeat(1, 10).reshape(2,-1)
-
+print("Empilhamento vertical: \n", np.vstack((a, b)))
 
 #6- Empilhe as matrizes horizontalmente:
 
 a = np.arange(10).reshape(2,-1)
 b = np.repeat(1, 10).reshape(2,-1)
+print("Empilhamento horizontal: \n", np.hstack((a, b)))
